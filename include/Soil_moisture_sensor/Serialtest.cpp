5c032f8e3bacac7e2fb64240311e6f4fab9977b3
Isla <smiler.isla@gmail.com>
2019-03-29 14:13:17 +0000

    Updated soil moisture code with example to print values

---
+++ include/Soil_moisture_sensor/Serialtest.cpp
@@ -0,0 +1,23 @@
+// Include libraries this sketch will use
+#include  <wiringPi.h>
+#include  <MCP342X.h>
+#include <stdio.h>
+
+// Instantiate objects used in this project
+MCP342X myADC;
+
+int main() {
+  printf("Starting up");
+
+  myADC.configure( MCP342X_MODE_CONTINUOUS |
+                   MCP342X_CHANNEL_1 |
+                   MCP342X_SIZE_16BIT |
+                   MCP342X_GAIN_1X
+                 );
+  while(1){
+  static int16_t  result;
+  myADC.startConversion();
+  myADC.getResult(&result); // conversion takes 63568us
+  printf("%d \n", result);
+  }
+}
