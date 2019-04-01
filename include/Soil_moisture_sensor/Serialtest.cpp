40cf8ab0767c3a7f471c1dc65623f08fe49fd782
Isla <smiler.isla@gmail.com>
2019-04-01 18:08:05 +0100

    Updated serial test code to sample data

--- include/Soil_moisture_sensor/Serialtest.cpp
+++ include/Soil_moisture_sensor/Serialtest.cpp
@@ -3,19 +3,50 @@
 #include <wiringPiI2C.h>
 #include <stdio.h>
 #include "MCP342X.h"
+#include "MCP342X.cpp"
 
 // Instantiate objects used in this project
 MCP342X myADC;
+static int intial_setup = wiringPiSetup();
+static int fd = wiringPiI2CSetup(0x68);
 
-int main() {
+using namespace std;
+
+int readData();
+
+int main(int argc, char** argv) {
   printf("Starting up\n\n");
   int configData = 0;
   configData = myADC.configure(MCP342X_MODE_CONTINUOUS, MCP342X_CHANNEL_1, MCP342X_SIZE_16BIT, MCP342X_GAIN_1X);
 
-  while(1){
-  static int16_t  result;
-  myADC.startConversion(configData);
-  myADC.getResult(&result); // conversion takes 63568us
-  printf("%d \n", result);
-  }
+  int counter = 0;
+
+  // decode arguments
+    if(argc < 3) {
+        printf("You must provide at least two arguments \n First, the number of samples to take,\n then the delay (in sec) between each reading\n \n ");
+        exit(0);
+    }
+    int c = atoi(argv[1]);
+
+    // now we have the correct number of args
+    int sleep_delay = atoi(argv[2]) * 1000 ; // convert to millisecs
+
+  for (int i = 1; i < c; ++i)
+  {
+        cout << "in the loop\n";
+        // read the data. . .
+        int result = readData();
+        usleep(sleep_delay);
+    }
+
+}
+
+int readData() {
+    // add the sensor stuff here
+    int result;
+    myADC.startConversion(configData);
+    printf("Result:");
+    myADC.getResult(&result); // conversion takes 63568us
+    printf("%d \n", result);
+    return 1;
 }
