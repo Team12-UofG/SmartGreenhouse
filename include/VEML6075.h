b4c7d37ee3c54a9a7378e9d07dc5f7d09032755f
Isla <smiler.isla@gmail.com>
2019-04-03 16:39:57 +0100

    Fixed errors in header files.

--- include/VEML6075.h
+++ include/VEML6075.h
@@ -51,33 +51,33 @@
 #endif
 
 /* VEML6075 slave address */
-#define VEML6075_ADDR                   (0x10) // I2C address
-#define VEML6075_DEVID                  (0x26) // Manufacture ID
+#define VEML6075_ADDR                   0x10 // I2C address
+#define VEML6075_DEVID                  0x26 // Manufacture ID
 
 /* Register definitions */
-#define VEML6075_CONF_REG               (0x00) // Configuration register
-#define VEML6075_UVA_DATA_REG           (0x07) // UVA raw measurement register
-#define VEML6075_REG_DUMMY              (0x08) // Dark current register
-#define VEML6075_UVB_DATA_REG           (0x09) // UVB raw measurement register
-#define VEML6075_UVCOMP1_DATA_REG       (0x0A) // Visible noise compensation register
-#define VEML6075_UVCOMP2_DATA_REG       (0x0B) // IR noisecompensation register
-#define VEML6075_ID_REG                 (0x0C) // Device ID register
+#define VEML6075_CONF_REG               0x00 // Configuration register
+#define VEML6075_UVA_DATA_REG           0x07 // UVA raw measurement register
+#define VEML6075_REG_DUMMY              0x08 // Dark current register
+#define VEML6075_UVB_DATA_REG           0x09 // UVB raw measurement register
+#define VEML6075_UVCOMP1_DATA_REG       0x0A // Visible noise compensation register
+#define VEML6075_UVCOMP2_DATA_REG       0x0B // IR noisecompensation register
+#define VEML6075_ID_REG                 0x0C // Device ID register
 
 /* Register value define : UV_CONF */
-#define VEML6075_CONF_SD_OFF            (0x00) // Power up
-#define VEML6075_CONF_SD_ON             (0x01) // Power down
-#define VEML6075_CONF_UV_AF_AUTO        (0x00) // Active force mode disabled (default)
-#define VEML6075_CONF_UV_AF_FORCE       (0x02) // Active force mode enabled
-#define VEML6075_CONF_UV_TRIG_NO        (0x00) // No trigger
-#define VEML6075_CONF_UV_TRIG_ONCE      (0x04) // Trigger measurement, clears by itself
-#define VEML6075_CONF_HD                (0x08) // High dynamic setting
-#define VEML6075_CONF_HD_OFF            (0x00) // Normal dynamic setting
-#define VEML6075_CONF_UV_IT_MASK        (0x70) // Mask off other config bits or 0x8F
-#define VEML6075_CONF_UV_IT_50MS        (0x00) // Integration time = 50ms
-#define VEML6075_CONF_UV_IT_100MS       (0x10) // Integration time = 100ms (default)
-#define VEML6075_CONF_UV_IT_200MS       (0x20) // Integration time = 200ms
-#define VEML6075_CONF_UV_IT_400MS       (0x30) // Integration time = 400ms
-#define VEML6075_CONF_UV_IT_800MS       (0x40) // Integration time = 800ms
+#define VEML6075_CONF_SD_OFF            0x00 // Power up
+#define VEML6075_CONF_SD_ON             0x01 // Power down
+#define VEML6075_CONF_UV_AF_AUTO        0x00 // Active force mode disabled (default)
+#define VEML6075_CONF_UV_AF_FORCE       0x02 // Active force mode enabled
+#define VEML6075_CONF_UV_TRIG_NO        0x00 // No trigger
+#define VEML6075_CONF_UV_TRIG_ONCE      0x04 // Trigger measurement, clears by itself
+#define VEML6075_CONF_HD                0x08 // High dynamic setting
+#define VEML6075_CONF_HD_OFF            0x00 // Normal dynamic setting
+#define VEML6075_CONF_UV_IT_MASK        0x70 // Mask off other config bits or 0x8F
+#define VEML6075_CONF_UV_IT_50MS        0x00 // Integration time = 50ms
+#define VEML6075_CONF_UV_IT_100MS       0x10 // Integration time = 100ms (default)
+#define VEML6075_CONF_UV_IT_200MS       0x20 // Integration time = 200ms
+#define VEML6075_CONF_UV_IT_400MS       0x30 // Integration time = 400ms
+#define VEML6075_CONF_UV_IT_800MS       0x40 // Integration time = 800ms
 #define VEML6075_CONF_DEFAULT           (VEML6075_CONF_UV_AF_AUTO | VEML6075_CONF_UV_TRIG_NO | VEML6075_CONF_UV_IT_100MS)
 
 /* Definition of coefficients used to calculate UV Index */
@@ -117,8 +117,15 @@ class UV_sensor {
   uint16_t _read_delay;
 
   // coefficients
-  float _uva_a, _uva_b, _uvb_c, _uvb_d, _uva_resp, _uvb_resp;
-  float _uva_calc, _uvb_calc, _uvi_calc;
+  float _uva_a;
+  float _uva_b;
+  float _uvb_c;
+  float _uvb_d;
+  float _uva_resp;
+  float _uvb_resp;
+  float _uva_calc;
+  float _uvb_calc;
+  float _uvi_calc;
 };
 
 #endif
