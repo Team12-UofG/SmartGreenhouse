5fb13d8999763f9ec70a12110775ffaa1a0525a4
jaimesrobb <46957448+jaimesrobb@users.noreply.github.com>
2019-04-08 14:19:50 +0100

    Update MCP342X.h
    Doxygen grouping.

--- include/Soil_sensor/MCP342X.h
+++ include/Soil_sensor/MCP342X.h
@@ -8,7 +8,7 @@
     MCP47X6 series of Analog-to-Digital converters which are connected
     via the I2C bus.
 
-    @brief MCP342X I2C device class header file
+    @brief MCP342X I2C device class header file.
     @detailed Based on Microchip datasheets for the following part numbers
         MCP3421, MCP3422, MCP3423, MCP3424, MCP3425, MCP3426, MCP3427, MCP3428
     These parts share a common programming interface
@@ -61,11 +61,13 @@
 #define MCP342X_A0FLT_A1VCC		0x6F
 //@}
 
+//@{
 /** Conversion mode definitions */
 #define MCP342X_MODE_ONESHOT	0x00
 #define MCP342X_MODE_CONTINUOUS	0x10
+//@}
 
-
+//@{
 /** Channel definitions */
 /** MCP3421 & MCP3425 have only the one channel and ignore this parameter
 * MCP3422, MCP3423, MCP3426 & MCP3427 have two channels and treat 3 & 4 as repeats of 1 & 2, respectively,
@@ -75,8 +77,9 @@
 #define	MCP342X_CHANNEL_3	0x40
 #define	MCP342X_CHANNEL_4	0x60
 #define	MCP342X_CHANNEL_MASK	0x60
+//@}
 
-
+//@{
 /** Sample size definitions - these also affect the sampling rate */
 /** 12-bit has a max sample rate of 240sps
 * 14-bit has a max sample rate of  60sps
@@ -87,15 +90,16 @@
 #define MCP342X_SIZE_16BIT	0x08
 #define MCP342X_SIZE_18BIT	0x0C
 #define MCP342X_SIZE_MASK  	0x0C
+//@}
 
-
-/** Programmable Gain definitions*/
+//@{
+/** Programmable gain definitions*/
 #define MCP342X_GAIN_1X	0x00
 #define MCP342X_GAIN_2X	0x01
 #define MCP342X_GAIN_4X	0x02
 #define MCP342X_GAIN_8X	0x03
 #define MCP342X_GAIN_MASK 0x03
-
+//@}
 
 /** /RDY bit definition */
 #define MCP342X_RDY	0x80
