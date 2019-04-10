4fd3762cf89c30d24ff3bf26ee37d098f4d08fa4
Isla <smiler.isla@gmail.com>
2019-04-10 13:18:01 +0100

    Temporary test file for measuring the elapsed time

---
+++ test/preliminary/Soil_sensor/temp.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>      // add "-lstdc++" to compile
+#include <unistd.h>
+#include <chrono>
+#include <wiringPi.h>
+#include <wiringPiI2C.h> // add "-lwiringPi" to compile
+
+
+int main(int argc, char** argv) {
+  auto start = std::chrono::high_resolution_clock::now();
+  configData = soil_sensor.configure();
+  wiringPiSetup();
+  pinMode (6, OUTPUT); // Setup pin 22 (GPIO 6) as output pin
+  digitalWrite(6, HIGH);
+  auto finish = std::chrono::high_resolution_clock::now();
+}
