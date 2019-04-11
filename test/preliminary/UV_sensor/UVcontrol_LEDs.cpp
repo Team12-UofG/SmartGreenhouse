d8d33d104eb7e33adc99b65342bf2b0d7f5e12b4
Isla <smiler.isla@gmail.com>
2019-04-11 18:07:52 +0100

    Updated to work without class for the LED matrix panel

--- test/preliminary/UV_sensor/UVcontrol_LEDs.cpp
+++ test/preliminary/UV_sensor/UVcontrol_LEDs.cpp
@@ -17,10 +17,7 @@
 #include <unistd.h>
 #include "../../../include/UV_sensor/VEML6075.h"
 #include "../../../include/UV_sensor/VEML6075.cpp"
-#include "../../../include/UV_sensor/LEDMATRIX.h"
-#include "../../../include/UV_sensor/LEDMATRIX.cpp"
 
-UV_sensor lightSensor;  // create UV sensor object
 LED_matrix led_panel;   // create LED matrix panel object
 
 int LED_pin = 26;       // GPIO 27 is pin 32
@@ -29,16 +26,17 @@ int UVI_threshold = 3;  // This is the UVI value at which the LEDs will turn on
 int main (){
 
   printf("UV Sensor with LED matrix test \n");
-  lightSensor.uvConfigure();          // configure sensor
-  led_panel.configurePanel(LED_pin);  // configure LED panel
+  wiringPiSetup();
+  pinmode(LED_pin, OUTPUT);
 
   float UV_calc = lightSensor.readUVI(); // UV value
   if (UV_calc < UVI_threshold) {
     printf("UV Index is too low: %f . Turning LED Growth Matrix on \n", UV_calc);
-    led_panel.on();
+    digitalWrite(LED_pin, HIGH);
   }
   else {
     led_panel.off();
     printf("UV Index is %f \n", UV_calc);
+    digitalWrite(LED_pin, LOW);
   }
 }
