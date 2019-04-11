07d54fe0b567563c9f5b19f779f86d7993468bf8
Isla <smiler.isla@gmail.com>
2019-04-11 18:42:28 +0100

    Minor bug fix

--- test/preliminary/Soil_sensor/SensorPump.cpp
+++ test/preliminary/Soil_sensor/SensorPump.cpp
@@ -41,7 +41,8 @@ int main(int argc, char** argv) {
    pinMode (water_pump, OUTPUT); // Setup pin 22 (GPIO 6) as output pin
 
    soil_sensor.startConversion(configData); // Start conversion
-   int result = soil_sensor.getResult(&result); // Read converted value
+   int result
+   result = soil_sensor.getResult(&result); // Read converted value
 
    if (result > dry_threshold){
      printf("Soil is too dry, pump is turning on");
