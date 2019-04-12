b6c5632deb53f7f1e446ceff179e8705693e0c83
AntonSaikia <43314966+AntonSaikia@users.noreply.github.com>
2019-04-13 01:06:30 +0530

    Create waterpump.cpp

---
+++ test/unit/manual_op/waterpump.cpp
@@ -0,0 +1,25 @@
+/**************************************************************************/
+/*!
+ *  @file waterpump.cpp
+ *  @author A. Saikia
+ *  @brief turns each of the outputs on in turn to check connection
+ *  @version 0.1
+ *  @date 2019-04-12
+ *  @copyright Copyright (c) 2019
+ *
+*/
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h> // add "-lwiringPi" to compile
+#include <stdio.h>
+#include <iostream>      // add "-lstdc++" to compile
+#include <unistd.h>
+
+int water_pump = 23;
+int main (){
+  wiringPiSetup();
+  pinMode(water_pump, OUTPUT);
+  digitalWrite(water_pump, HIGH);
+  delay(10000);         // pump is on for 10 seconds
+  digitalWrite(water_pump, LOW);
+}
