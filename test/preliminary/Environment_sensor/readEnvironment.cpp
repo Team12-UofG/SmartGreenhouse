13efcb7b314bbf86e618901d0d89f623e058cb73
Isla <smiler.isla@gmail.com>
2019-04-12 12:48:43 +0100

    Reverted back to original, not possible to use wiringPi with the BME680 C driver

--- test/preliminary/Environment_sensor/readEnvironment.cpp
+++ test/preliminary/Environment_sensor/readEnvironment.cpp
@@ -1,18 +1,21 @@
-/**
- * @file SGH_TPAQ.c
+/**************************************************************************/
+
+/*!
+ *  @file readEnvironment.cpp
+ *  @authors A. Saikia and I. Mitchell
+ *  @brief takes one reading of the environment sensor and prints the values
+ *  @version 0.1
+ *  @date 2019-04-11
+ *  @copyright Copyright (c) 2019
  *
- * @brief I2C communication with Bosch BME680 Temperature, Humidity and Air Quality Sensor
- *
- *
- * Sensor Data is exported to MySQL database
- * Application Note
- *
- * @author Anton Saikia (based on example by twartzek and Boschsensortech)
- *
- **/
+*/
+
 #include <wiringPi.h>
 #include <wiringPiI2C.h> // add "-lwiringPi" to compile
 #include <stdio.h>
+#include <iostream>      // add "-lstdc++" to compile
+#include <unistd.h>
+#include <mysql/mysql.h> // add "-lmysqlclient" to compile
 #include <stdlib.h>
 #include <time.h>
 #include <linux/i2c-dev.h>
@@ -21,16 +24,59 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include "../../../include/Environment_sensor/bme680.h"
+#include "../../../include/Soil_sensor/MCP342X.h"
+#include "../../../include/Soil_sensor/MCP342X.cpp"
+#include "../../../include/UV_sensor/VEML6075.h"
+#include "../../../include/UV_sensor/VEML6075.cpp"
 
 /*! @brief Our destination time zone */
 #define     DESTZONE    "TZ=Europe/London"       // Our destination time zone
 
+/*!
+ * @brief Instantiate objects used in this project
+ */
+MCP342X soilSensor;
+int Soil_configData = 0;;
+UV_sensor lightSensor; // create sensor
+int readData();
+
 /*! @brief I2C Linux device handle.
 */
-static int setupI2C_BME680 = wiringPiSetup();
-static int fd_env = wiringPiI2CSetup(0x76);
+int g_i2cFid;
+
+/*!
+    @brief Open the Linux device.
+*/
+void i2cOpen()
+{
+	g_i2cFid = open("/dev/i2c-1", O_RDWR);
+	if (g_i2cFid < 0) {
+		perror("i2cOpen");
+		exit(1);
+	}
+}
+
+/*!
+    @brief Close the Linux device.
+*/
+void i2cClose()
+{
+	close(g_i2cFid);
+}
+
+/*!
+    @brief Set the I2C slave address for all subsequent I2C device transfers.
+    @param address[in] : 12C slave address
+*/
+void i2cSetAddress(int address)
+{
+	if (ioctl(g_i2cFid, I2C_SLAVE, address) < 0) {
+		perror("i2cSetAddress");
+		exit(1);
+	}
+}
+
 
 /*!
     @brief Set the user delay in milliseconds.
@@ -41,46 +87,85 @@ void user_delay_ms(uint32_t period)
     sleep(period/1000);
 }
 
-
 /*!
-    @brief Writes measurements to output file specified.
-    @param outputFile
-    @param tm
-    @param bme680_field_data
+    @brief Read I2C information.
+    @param  dev_id
+    @param  reg_addr
+    @param  reg_data
+    @param  len
 */
-void write2file(char *outputFile, struct tm tm, struct bme680_field_data data)
+int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
 {
-	// Write measurement to output file if specified.
-	if(outputFile != NULL)
-	{
-		FILE *f = fopen(outputFile, "a");
-		if (f == NULL)
-		{
-			printf("Error opening file!\n");
-			//exit(1);
-		}
-		else
-		{
-			fprintf(f,"%d-%02d-%02d %02d:%02d:%02d ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-			fprintf(f,"T: %.2f degC, P: %.2f hPa, H: %.2f %%rH", data.temperature / 100.0f,
-					data.pressure / 100.0f, data.humidity / 1000.0f );
-			fprintf(f,", G: %d Ohms", data.gas_resistance);
-			fprintf(f,"\r\n");
-			fclose(f);
-		}
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+    uint8_t reg[1];
+	reg[0]=reg_addr;
 
+ 	if (write(g_i2cFid, reg, 1) != 1) {
+		perror("user_i2c_read_reg");
+		rslt = 1;
 	}
+
+	if (read(g_i2cFid, reg_data, len) != len) {
+		perror("user_i2c_read_data");
+		rslt = 1;
+	}
+
+    return rslt;
 }
 
 /*!
-    @brief Main function.
-    @param argc
-    @param argv
+    @brief Write I2C information.
+    @param  dev_id
+    @param  reg_addr
+    @param  reg_data
+    @param  len
 */
-int main(int argc, char *argv[] )
+
+int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
 {
+    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+
+	uint8_t reg[16];
+    reg[0]=reg_addr;
+
+    for (int i=1; i<len+1; i++)
+       reg[i] = reg_data[i-1];
+
+    if (write(g_i2cFid, reg, len+1) != len+1) {
+		perror("user_i2c_write");
+		rslt = 1;
+	        exit(1);
+	}
+
+    return rslt;
+}
+
+/*!
+ * @brief main progam
+ */
+
+int main(int argc, char *argv[] ) {
+
+	printf("Simple Test \n");
+
+	lightSensor.uvConfigure(); // configure sensor
+
+	Soil_configData = soilSensor.configure();
+
+        uint8_t SM;
+	soilSensor.startConversion(Soil_configData); // Start conversion
+	SM = soilSensor.getResult(&SM); // Read converted value
+	printf("Soil moisture reading: %d \n", SM);
+
+	float UV_calc = lightSensor.readUVI(); // UV value - this is the output we want
+	printf("UV Index reading: %f \n", UV_calc);
+
+	// initialize connection to MySQL database
+        MYSQL *mysqlConn;
+        MYSQL_RES result;
+        MYSQL_ROW row;
+        mysqlConn = mysql_init(NULL);
 	char buff[1024];
-	int j;
 
 	// create lock file first
 	FILE *f = fopen("~bme680i2c.lock", "w");
@@ -93,45 +178,48 @@ int main(int argc, char *argv[] )
 	fclose(f);
 
 	int delay = 3;
-	int nMeas = 3;
+	int nMeas = 1;
 	char *outputFile = NULL;
 
 	// Input argument parser
 	if( argc == 2 ) {
 		delay = strtol(argv[1], NULL, 10);
+
 	}
+
 	else if( argc == 3 ) {
 		delay = strtol(argv[1], NULL, 10);
 		nMeas = strtol(argv[2], NULL, 10);
 	}
+
 	else if( argc == 4 ) {
 		delay = strtol(argv[1], NULL, 10);
 		nMeas = strtol(argv[2], NULL, 10);
 		outputFile = argv[3];
 	}
-	else {
-	}
-
 
 	printf("** UofG Smartgreenhouse Environment measurements using BME680 **\n");
 
 	time_t t = time(NULL);
-    putenv(DESTZONE);               // Switch to destination time zone
+	putenv(DESTZONE);               // Switch to destination time zone
 
+	// open Linux I2C device
+	i2cOpen();
 
-  // init device
+	// set address of the BME680
+	i2cSetAddress(0x76);
+
+	// init device
 	struct bme680_dev gas_sensor;
-
 	gas_sensor.dev_id = BME680_I2C_ADDR_SECONDARY;
 	gas_sensor.intf = BME680_I2C_INTF;
-	gas_sensor.read = wiringPiI2CRead(fd_env);
-	gas_sensor.write = wiringPiI2CWrite(fd_env);
+	gas_sensor.read = user_i2c_read;
+	gas_sensor.write = user_i2c_write;
 	gas_sensor.delay_ms = user_delay_ms;
 
 	int8_t rslt = BME680_OK;
 	rslt = bme680_init(&gas_sensor);
-
-  uint8_t set_required_settings;
+	uint8_t set_required_settings;
 
 	/* Set the temperature, pressure and humidity settings */
 	gas_sensor.tph_sett.os_hum = BME680_OS_2X;
@@ -141,6 +229,7 @@ int main(int argc, char *argv[] )
 
 	/* Set the remaining gas sensor settings and link the heating profile */
 	gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
+
 	/* Create a ramp heat waveform in 3 steps */
 	gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
 	gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
@@ -159,24 +248,20 @@ int main(int argc, char *argv[] )
 	/* Set the power mode */
 	rslt = bme680_set_sensor_mode(&gas_sensor);
 
-
-
 	/* Get the total measurement duration so as to sleep or wait till the
 	 * measurement is complete */
 	uint16_t meas_period;
 	bme680_get_profile_dur(&meas_period, &gas_sensor);
 	user_delay_ms(meas_period + delay*1000); /* Delay till the measurement is ready */
 
-
-  struct bme680_field_data data;
+    struct bme680_field_data data;
 
 	struct tm tm = *localtime(&t);
 
 	int i=0;
 	int backupCounter = 0;
 
-	while(i<nMeas && backupCounter < nMeas+5) {
-
+	while(i<nMeas && backupCounter < nMeas+3) {
 		// Get sensor data
 		rslt = bme680_get_sensor_data(&data, &gas_sensor);
 
@@ -190,8 +275,14 @@ int main(int argc, char *argv[] )
 					data.pressure / 100.0f, data.humidity / 1000.0f );
 			printf(", G: %d Ohms", data.gas_resistance);
 			printf("\r\n");
-			write2file(outputFile, tm, data);
 			i++;
+	}
+
+		// Measurement to MYSQL database
+		if(mysql_real_connect(mysqlConn,"localhost", "UOG_SGH", "test", "SGH_TPAQ", 0, NULL, 0)!=NULL)
+		{
+                        snprintf(buff, sizeof buff, "INSERT INTO TPAQ VALUES ('', '%d', '%f', '%02d', '%02d', '%02d', '%02d', '%02d', '%.2f','%.2f','%.2f','%d');",SM, UV_calc, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, data.temperature / 100.0f, data.pressure / 100.0f, data.humidity / 1000.0f, data.gas_resistance );
+	                mysql_query(mysqlConn, buff);
 		}
 
 		// Trigger a meausurement
@@ -199,12 +290,16 @@ int main(int argc, char *argv[] )
 
 		// Wait for a measurement to complete
 		user_delay_ms(meas_period + delay*1000); /* Wait for the measurement to complete */
-
-		backupCounter++;
+	   	backupCounter++;
 	}
 
-
 	printf("** End of measurement **\n");
+
+	// close MySQL
+	mysql_close(mysqlConn);
+
+  	// close Linux I2C device
+	i2cClose();
 
 	// delete lock file
 	remove("~bme680i2c.lock");
