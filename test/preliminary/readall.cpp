327cb1de0200d95f15b91a5a1cfee82802381365
Isla <smiler.isla@gmail.com>
2019-04-12 16:11:58 +0100

    Fix segmentation fault

--- test/preliminary/readall.cpp
+++ test/preliminary/readall.cpp
@@ -76,7 +76,7 @@ void configureBME(int delay, int nMeas);
 int main (int argc, char *argv[]){
   int delay = 3;
   int nMeas = 1;
-  
+
   // Input argument parser
   if( argc == 2 ) {
     delay = strtol(argv[1], NULL, 10);
@@ -92,46 +92,8 @@ int main (int argc, char *argv[]){
     nMeas = strtol(argv[2], NULL, 10);
   }
 
-
-  time_t t = time(NULL);
-
   configureBME(delay, nMeas);
-  struct bme680_field_data data;
-  struct bme680_dev gas_sensor;
-  struct tm tm = *localtime(&t);
 
-	int i=0;
-	int backupCounter = 0;
-
-	while(i<nMeas && backupCounter < nMeas+3) {
-		// Get sensor data
-		int8_t rslt = bme680_get_sensor_data(&data, &gas_sensor);
-    uint16_t meas_period;
-		// Avoid using measurements from an unstable heating setup
-		if(data.status & BME680_HEAT_STAB_MSK)
-		{
-			t = time(NULL);
-			tm = *localtime(&t);
-			printf("%d-%02d-%02d %02d:%02d:%02d ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-			printf("T: %.2f degC, P: %.2f hPa, H: %.2f %%rH", data.temperature / 100.0f,
-					data.pressure / 100.0f, data.humidity / 1000.0f );
-			printf(", G: %d Ohms", data.gas_resistance);
-			printf("\r\n");
-			i++;
-	   }
-
-		// Trigger a meausurement
-		rslt = bme680_set_sensor_mode(&gas_sensor); /* Trigger a measurement */
-
-		// Wait for a measurement to complete
-		user_delay_ms(meas_period + delay*1000); /* Wait for the measurement to complete */
-	  backupCounter++;
-	}
-
-	printf("** End of measurement **\n");
-
-  	// close Linux I2C device
-	i2cClose();
 /*
   printf("Test to read all of the sensor values \n");
 
@@ -153,55 +115,85 @@ int main (int argc, char *argv[]){
 
 
 void configureBME(int delay, int nMeas){
-  // open Linux I2C device
-  i2cOpen();
-
-  // set address of the BME680
-  i2cSetAddress(0x76);
-
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
   // init device
-  struct bme680_dev gas_sensor;
-  gas_sensor.dev_id = BME680_I2C_ADDR_SECONDARY;
-  gas_sensor.intf = BME680_I2C_INTF;
-  gas_sensor.read = user_i2c_read;
-  gas_sensor.write = user_i2c_write;
-  gas_sensor.delay_ms = user_delay_ms;
+	struct bme680_dev gas_sensor;
+	gas_sensor.dev_id = BME680_I2C_ADDR_SECONDARY;
+	gas_sensor.intf = BME680_I2C_INTF;
+	gas_sensor.read = user_i2c_read;
+	gas_sensor.write = user_i2c_write;
+	gas_sensor.delay_ms = user_delay_ms;
 
-  int8_t rslt = BME680_OK;
-  rslt = bme680_init(&gas_sensor);
-  uint8_t set_required_settings;
+	int8_t rslt = BME680_OK;
+	rslt = bme680_init(&gas_sensor);
+	uint8_t set_required_settings;
 
-  /* Set the temperature, pressure and humidity settings */
-  gas_sensor.tph_sett.os_hum = BME680_OS_2X;
-  gas_sensor.tph_sett.os_pres = BME680_OS_4X;
-  gas_sensor.tph_sett.os_temp = BME680_OS_8X;
-  gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+	/* Set the temperature, pressure and humidity settings */
+	gas_sensor.tph_sett.os_hum = BME680_OS_2X;
+	gas_sensor.tph_sett.os_pres = BME680_OS_4X;
+	gas_sensor.tph_sett.os_temp = BME680_OS_8X;
+	gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
 
-  /* Set the remaining gas sensor settings and link the heating profile */
-  gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
+	/* Set the remaining gas sensor settings and link the heating profile */
+	gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
 
-  /* Create a ramp heat waveform in 3 steps */
-  gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
-  gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
+	/* Create a ramp heat waveform in 3 steps */
+	gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
+	gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
 
-  /* Select the power mode */
-  /* Must be set before writing the sensor configuration */
-  gas_sensor.power_mode = BME680_FORCED_MODE;
+	/* Select the power mode */
+	/* Must be set before writing the sensor configuration */
+	gas_sensor.power_mode = BME680_FORCED_MODE;
 
-  /* Set the required sensor settings needed */
-  set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL
-    | BME680_GAS_SENSOR_SEL;
+	/* Set the required sensor settings needed */
+	set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL
+		| BME680_GAS_SENSOR_SEL;
 
-  /* Set the desired sensor configuration */
-  rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
+	/* Set the desired sensor configuration */
+	rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
 
-  /* Set the power mode */
-  rslt = bme680_set_sensor_mode(&gas_sensor);
+	/* Set the power mode */
+	rslt = bme680_set_sensor_mode(&gas_sensor);
 
-  /* Get the total measurement duration so as to sleep or wait till the
-   * measurement is complete */
-  uint16_t meas_period;
-  bme680_get_profile_dur(&meas_period, &gas_sensor);
+	/* Get the total measurement duration so as to sleep or wait till the
+	 * measurement is complete */
+	uint16_t meas_period;
+	bme680_get_profile_dur(&meas_period, &gas_sensor);
+	user_delay_ms(meas_period + delay*1000); /* Delay till the measurement is ready */
+
+  struct bme680_field_data data;
+
+	struct tm tm = *localtime(&t);
+
+	int i=0;
+	int backupCounter = 0;
+
+	while(i<nMeas && backupCounter < nMeas+3) {
+		// Get sensor data
+		rslt = bme680_get_sensor_data(&data, &gas_sensor);
+
+		// Avoid using measurements from an unstable heating setup
+		if(data.status & BME680_HEAT_STAB_MSK)
+		{
+			t = time(NULL);
+			tm = *localtime(&t);
+			printf("%d-%02d-%02d %02d:%02d:%02d ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+			printf("T: %.2f degC, P: %.2f hPa, H: %.2f %%rH", data.temperature / 100.0f,
+					data.pressure / 100.0f, data.humidity / 1000.0f );
+			printf(", G: %d Ohms", data.gas_resistance);
+			printf("\r\n");
+			i++;
+	}
+
+		// Trigger a meausurement
+		rslt = bme680_set_sensor_mode(&gas_sensor); /* Trigger a measurement */
+
+		// Wait for a measurement to complete
+		user_delay_ms(meas_period + delay*1000); /* Wait for the measurement to complete */
+	   	backupCounter++;
+	}
+  i2cClose();
 }
 
 /*!
