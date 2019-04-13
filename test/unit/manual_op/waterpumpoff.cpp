8dc4ca06d10c1b852ce6b6ea09b90f3075057f02
jaimesrobb <46957448+jaimesrobb@users.noreply.github.com>
2019-04-13 20:06:46 +0100

    Update waterpumpoff.cpp
    doxygen comments.

--- test/unit/manual_op/waterpumpoff.cpp
+++ test/unit/manual_op/waterpumpoff.cpp
@@ -2,7 +2,7 @@
 /*!
  *  @file waterpumpoff.cpp
  *  @author A. Saikia
- *  @brief turns water pump off
+ *  @brief Turns water motor off.
  *  @version 0.1
  *  @date 2019-04-12
  *  @copyright Copyright (c) 2019
@@ -15,7 +15,9 @@
 #include <iostream>      // add "-lstdc++" to compile
 #include <unistd.h>
 
+/** @brief GPIO pin of motor. */
 int water_pump = 23;
+/** @brief Main function. */
 int main (){
   wiringPiSetup();
 }
