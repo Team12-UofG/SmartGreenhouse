e0b7e2e63f98072ba4f9e1a367c768716e30cc50
jaimesrobb <46957448+jaimesrobb@users.noreply.github.com>
2019-04-13 01:15:12 +0100

    Update LEDMATRIX.h
    Adding Doxygen comments.

--- include/UV_sensor/LEDMATRIX.h
+++ include/UV_sensor/LEDMATRIX.h
@@ -1,6 +1,14 @@
+/**
+* @file  LEDMATRIX.h
+* @brief  Header for control of LED panels.
+*/
+
 #ifndef _LEDMATRIX_H
 #define _LEDMATRIX_H
 
+/*! 
+* @brief Control of LED panels.
+*/
 class LED_matrix {
  public:
   void configurePanel(int LED_pin1, int LED_pin2);
@@ -12,7 +20,13 @@ class LED_matrix {
   void off(void);
 
  private:
+ /*! 
+ @brief Control of LED panel at pin 1.
+ */
    int LED_pin1;
+ /*! 
+ @brief Control of LED panel at pin 2.
+ */
    int LED_pin2;
 };
 
